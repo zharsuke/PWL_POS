2b89f4ccd7f2f87a60292303c8834dc07d57fdc7
zharsuke <alazharisqi@gmail.com>
2024-03-09 20:02:25 +0700

    feat: Praktikum 2.1 – Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,15 +11,31 @@ class UserController extends Controller
 {
     public function index() {
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager Tiga',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'username' => 'customer 1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 1
+        // ];
+        // UserModel::insert($data);
 
-        $user = UserModel::all();
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager Tiga',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all();
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,14 +15,14 @@
             <th>Name</th>
             <th>ID Level User</th>
         </tr>
-        @foreach ($data as $d)
+        {{-- @foreach ($data as $data) --}}
         <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
-        @endforeach
+        {{-- @endforeach --}}
     </table>
 </body>
 </html>
