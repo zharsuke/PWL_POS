fcfe86b01596f87be1d48da1322a31a5ba7da165
zharsuke <alazharisqi@gmail.com>
2024-03-09 17:32:34 +0700

    feat: Practicum 1 - fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,19 +11,13 @@ class UserController extends Controller
 {
     public function index() {
 
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
         $data = [
-            'nama' => 'Pelanggan pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager Tiga',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
-
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,10 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama'
+    ];
 }
